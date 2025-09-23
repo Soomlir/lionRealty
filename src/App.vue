5d7fca8334da58b5a7d60194171038d3332728d1
Soomlir <Soomlir@yandex.ru>
2025-09-23 10:12:58 +0300

    deploy add

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,30 @@ import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
-  <div class="header-wrap">
-    <HeaderComponent />
-    <AboutComponent />
+  <div class="app-wrap">
+    <div class="header-wrap">
+      <HeaderComponent />
+      <AboutComponent />
+    </div>
+    <PhotosComponent />
+    <PresentComponent />
+    <DetailsComponent />
+    <CreditComponent />
+    <AddressComponent />
+    <FooterComponent />
   </div>
-  <PhotosComponent />
-  <PresentComponent />
-  <DetailsComponent />
-  <CreditComponent />
-  <AddressComponent />
-  <FooterComponent />
 </template>
 
 <style lang="scss" scoped>
+.app-wrap {
+  width: 320px;
+  margin: 0 auto;
+
+  @media (min-width: 1440px) {
+    width: 1440px;
+  }
+}
+
 .header-wrap {
   padding: 0 30px 77px;
   background-image: url("../images/header-bg-mobile.jpg");
